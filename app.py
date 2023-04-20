2f748e863d9adfab529ba228445b3dd3cc18be10
dev-rafa1707 <rafa1707@gmail.com>
2023-04-20 18:00:51 -0300

    corrige bug response Nonetype a partir do link url

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-html = requests.get('https://www.climatempo.com.br/').text
+html = requests.get('https://www.climatempo.com.br/previsao-do-tempo/cidade/1310/franciscobeltrao-pr').text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-# tempMin = soup.find(id='min-temp-1')
-# print(tempMin.text)
+tempMin = soup.find(id='min-temp-1')
+print(tempMin.text)
 
 tempMax = soup.find(id='max-temp-1')
 print(tempMax.text)
