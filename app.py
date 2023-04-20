3fa37cb84e28150abe4fbae1db08c1e4f4edd2b9
dev-rafa1707 <rafa1707@gmail.com>
2023-04-20 16:47:42 -0300

    add max temp

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ html = requests.get('https://www.climatempo.com.br/')
 
 soup = BeautifulSoup(html, 'html.parser')
 
-tempMin = soup.find(id='min-temp-1')
-print(tempMin.text)
+# tempMin = soup.find(id='min-temp-1')
+# print(tempMin.text)
+
+tempMax = soup.find(id='max-temp-1')
+print(tempMax.text)
