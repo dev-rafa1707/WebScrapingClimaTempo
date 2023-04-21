73aa908f49dfcd1beb415f7b8e037e1cc850499a
dev-rafa1707 <rafa1707@gmail.com>
2023-04-21 10:24:37 -0300

    add local ao scrapping

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ html = requests.get('https://www.climatempo.com.br/previsao-do-tempo/cidade/1310
 
 soup = BeautifulSoup(html, 'html.parser')
 
+
+local = soup.find(class_="-bold -font-18 -dark-blue _margin-r-10 _margin-b-sm-5")
+print(f'Localização: {local}')
+
 tempMin = soup.find(id='min-temp-1')
 print(f'A temperatura mínima é {tempMin.text}')
 
@@ -12,4 +16,6 @@ tempMax = soup.find(id='max-temp-1')
 print(f'A temperatura máxima é {tempMax.text}')
 
 resumo = soup.find(class_= '-gray -line-height-24 _center')
-print(f'Resumo: {resumo.text}')
+resumo = resumo.text.strip()
+print(f'Resumo: {resumo}')
+
